29ec5f77dc8a0c2a9611ad0d227f3321c7cc2ed9
DecayCorpse <decay4it@gmail.com>
2022-09-06 16:30:36 +0300

    написал программу для вызова ментов. вроде как домашку выполнил, но хотел чтоб еще и текст выводился.

--- src/ThirdPart/ThirdPart.java
+++ src/ThirdPart/ThirdPart.java
@@ -1,13 +1,16 @@
 package ThirdPart;
 
+import java.util.Scanner;
+
 public class ThirdPart {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.println("Hello, its police department. To call the police write your address :");
 
         int number = sc.nextInt();
 
-        System.out.println("Спасибо! Вы ввели число " + number);
+        System.out.println("Thanks. the squad went to " + number);
 
     }
+}
