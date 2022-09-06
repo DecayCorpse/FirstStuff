7cca6a023a864af740bee0c26e3b2d7338600f0c
DecayCorpse <decay4it@gmail.com>
2022-09-06 16:00:15 +0300

    воспользовался методом scanner, но я так понимаб что это не совсем то  что от меня требовалось

--- src/SeccondPart/SeccondPart.java
+++ src/SeccondPart/SeccondPart.java
@@ -1,4 +1,18 @@
 package SeccondPart;
 
-public class SeccondPart
+import java.util.Scanner;
+
+public class SeccondPart {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner ("текст 1\n"+
+        "текст 2\n" +
+        "текст 3") ;
+    String s = scanner.nextLine();
+    System.out.println (s);
+    s = scanner.nextLine();
+    System.out.println (s);
+    s = scanner.nextLine();
+    System.out.println (s);
+
+    }
 }
