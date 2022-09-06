53dcc1754c5dbad3d50d2ef8ed98815541b9a537
DecayCorpse <decay4it@gmail.com>
2022-09-06 16:13:08 +0300

    воспользовался классом scanner, но я так понимаю что это не совсем то  что от меня требовалось

---
+++ src/ThirdPart/ThirdPart.java
@@ -0,0 +1,13 @@
+package ThirdPart;
+
+public class ThirdPart {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+
+        int number = sc.nextInt();
+
+        System.out.println("Спасибо! Вы ввели число " + number);
+
+    }
